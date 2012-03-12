9ac54151cda4926c4de502b0996b7ff392bc5b10
David Stosik <davidstosik@49385.no-reply.drupal.org>
2012-03-12 23:59:46 +0900

    Fixed "Return to form" AJAX link. Fixes #1477116 .

--- js/webform_ajax.js
+++ js/webform_ajax.js
@@ -4,11 +4,11 @@ Drupal.behaviors.webform_ajax = {
   attach: function (context,settings) {
 
     // Bind Ajax behaviors to all items showing the class.
-    $.each(Drupal.settings.webform_ajax, function(id, setting) {
+    $.each(Drupal.settings.webform_ajax.wrappers, function(id, setting) {
       $('#' + id + ' .links a:not(.ajax-processed)').addClass('ajax-processed').each(function () {
         // Fully fill element_settings, as Drupal's ajax.js seems not to merge default correctly.
         var element_settings = {
-          url: 'webform_ajax/return_webform/' + setting.nid,
+          url: Drupal.settings.webform_ajax.url + '/' + setting.nid,
           event: 'mousedown',
           prevent: 'click',
           keypress: false,
