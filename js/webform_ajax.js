9fb52c3ea7f46e3dc59f01c3e340ebf586dfb528
David Stosik <davidstosik@49385.no-reply.drupal.org>
2012-03-21 11:39:20 +0900

    Fix JS use of each(), to apply the appropriate AJAX handlers to confirmation page's "Go back to the form" link.

--- js/webform_ajax.js
+++ js/webform_ajax.js
@@ -4,30 +4,30 @@ Drupal.behaviors.webform_ajax = {
   attach: function (context, settings) {
 
     // Bind Ajax behaviors to all items showing the class.
-    $.each(settings.webform_ajax.wrappers, function(id, setting) {
-      $('.links a:not(.ajax-processed)', context).addClass('ajax-processed').each(function () {
-        // Fully fill element_settings, as Drupal's ajax.js seems not to merge default correctly.
-        var element_settings = {
-          url: settings.webform_ajax.url + '/' + setting.nid + '/' + setting.html_id,
-          event: 'mousedown',
-          prevent: 'click',
-          keypress: false,
-          selector: '#' + id,
-          effect: 'none',
-          speed: 'none',
-          method: 'replaceWith',
-          wrapper: id,
-          progress: {
-            type: 'throbber',
-            message: ''
-          },
-          submit: {
-            'js': true
-          }
-        };
+    $('.links a:not(.ajax-processed)', context).addClass('ajax-processed').each(function () {
+      // Fully fill element_settings, as Drupal's ajax.js seems not to merge default correctly.
+      wrapper_id = $(this).parents('[id^=webform-ajax-wrapper]').attr('id');
+      setting = settings.webform_ajax.wrappers[wrapper_id];
+      var element_settings = {
+        url: settings.webform_ajax.url + '/' + setting.nid + '/' + setting.html_id,
+        event: 'mousedown',
+        prevent: 'click',
+        keypress: false,
+        selector: '#' + setting.html_id,
+        effect: 'none',
+        speed: 'none',
+        method: 'replaceWith',
+        wrapper: setting.html_id,
+        progress: {
+          type: 'throbber',
+          message: ''
+        },
+        submit: {
+          'js': true
+        }
+      };
 
-        Drupal.ajax[id] = new Drupal.ajax(id, this, element_settings);
-      });
+      Drupal.ajax[setting.html_id] = new Drupal.ajax(setting.html_id, this, element_settings);
     });
   }
 
