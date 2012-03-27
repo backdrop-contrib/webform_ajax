d23b5f6ecf106c84df65fcffbc0bb003f472ff66
David Stosik <davidstosik@49385.no-reply.drupal.org>
2012-03-27 17:33:51 +0900

    Replace the drupal_goto in an AJAX request by an AJAX response that generates a new AJAX request to the wanted URL, in order to retrieve a fresh instance of the webform. Fixes #1489826 .

--- js/webform_ajax.js
+++ js/webform_ajax.js
@@ -1,34 +1,58 @@
 (function ($) {
 
 Drupal.behaviors.webform_ajax = {
+
   attach: function (context, settings) {
 
-    // Bind Ajax behaviors to all items showing the class.
+    // Fully declare element_settings, as Drupal's ajax.js seems not to merge defaults correctly.
+    var ajax_element_settings_defaults = {
+      url: settings.webform_ajax.url + '/',
+      event: 'mousedown',
+      prevent: 'click',
+      keypress: false,
+      selector: '#',
+      effect: 'none',
+      speed: 'none',
+      method: 'replaceWith',
+      wrapper: '',
+      progress: {
+        type: 'throbber',
+        message: ''
+      },
+      submit: {
+        'js': true
+      }
+    };
+
+    // Bind Ajax behaviors to Webform confirmation screen's "Go back to form" link.
     $('.links a:not(.ajax-processed)', context).addClass('ajax-processed').each(function () {
-      // Fully fill element_settings, as Drupal's ajax.js seems not to merge default correctly.
       wrapper_id = $(this).parents('[id^=webform-ajax-wrapper]').attr('id');
       setting = settings.webform_ajax.wrappers[wrapper_id];
-      var element_settings = {
-        url: settings.webform_ajax.url + '/' + setting.nid + '/' + setting.html_id,
-        event: 'mousedown',
-        prevent: 'click',
-        keypress: false,
-        selector: '#' + setting.html_id,
-        effect: 'none',
-        speed: 'none',
-        method: 'replaceWith',
-        wrapper: setting.html_id,
-        progress: {
-          type: 'throbber',
-          message: ''
-        },
-        submit: {
-          'js': true
-        }
-      };
 
-      Drupal.ajax[setting.html_id] = new Drupal.ajax(setting.html_id, this, element_settings);
+      var element_settings = ajax_element_settings_defaults;
+      element_settings.url += setting.nid + '/' + setting.wrapper_id;
+      element_settings.selector += setting.wrapper_id;
+      element_settings.wrapper += setting.wrapper_id;
+
+      Drupal.ajax[setting.wrapper_id] = new Drupal.ajax(setting.wrapper_id, this, element_settings);
     });
+
+    // If module set a 'reload' setting, then retrieve the Webform with AJAX.
+    if (setting = settings.webform_ajax.reload) {
+      var element_settings = ajax_element_settings_defaults;
+      element_settings.url += setting.nid + '/' + setting.wrapper_id;
+      element_settings.selector += setting.wrapper_id;
+      element_settings.wrapper += setting.wrapper_id;
+
+      button = $('#' + setting.button_id, context);
+
+      Drupal.ajax[setting.wrapper_id] = new Drupal.ajax(setting.wrapper_id, button, element_settings);
+      Drupal.ajax[setting.wrapper_id].eventResponse(button, null);
+
+      // Remove the reload setting to avoid looping.
+      delete settings.webform_ajax.reload;
+    }
+
   }
 
 };
